96da9e832fa22420796820c22e317b4390d74f05
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-10-07 22:17:32 -0300

    proper update cleanup + storage fixes
    Fixes for fuck-ups introduced in 1.4.0... it's been a bad day.

--- src/background/runtime.js
+++ src/background/runtime.js
@@ -14,3 +14,16 @@ browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 	// triggered by options page script
 	if (msg) sendResponse(settings.all);
 });
+
+browser.runtime.onInstalled.addListener(d => {
+	if (d.reason === 'update') {
+		const rx = /^0|1\.[0-3]\./;
+		if (rx.test(d.previousVersion)) {
+			browser.storage.sync.get(settings.all).then(rs => {
+				settings.all = rs;
+				browser.storage.sync.clear();
+				browser.storage.local.set(settings.all);
+			});
+		}
+	}
+});
--- src/background/storage.js
+++ src/background/storage.js
@@ -1,7 +1,5 @@
-browser.storage.sync.get(settings.all).then(rs => {
-	if (rs.sync) settings.all = rs;
-	else browser.storage.local.get(settings.all).then(rl => {settings.all = rl});
-}).catch((e) => console.log(`True-Sight: ${e}`));
+settings.storage.get(settings.all).then(r => {settings.all = r})
+	.catch((e) => console.log(`True-Sight: ${e}`));
 
 browser.storage.onChanged.addListener((changes, area) => {
 	const obj = {};
@@ -9,4 +7,4 @@ browser.storage.onChanged.addListener((changes, area) => {
 		if (changes[i].hasOwnProperty('newValue')) obj[i] = changes[i].newValue;
 	}
 	settings.all = obj;
-}).catch((e) => console.log(`True-Sight: ${e}`));
+});
