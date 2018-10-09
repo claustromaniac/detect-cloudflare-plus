8ab2e52e47b8c656ffd6cb46d34ec8471a26ba70
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-10-09 15:03:14 -0300

    retouch d32c701

--- src/options/options.js
+++ src/options/options.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	browser.runtime.sendMessage(true).then(settings => {
 		const keys = Object.keys(settings);
 		for (const i of keys) document.getElementById(i).checked = settings[i];
-		document.querySelector("form").addEventListener("submit", async e => {
+		document.querySelector("form").addEventListener("submit", e => {
 			e.preventDefault();
 			const obj = {};
 			for (const i of keys) obj[i] = document.getElementById(i).checked;
-			obj.sync ? await browser.storage.sync.set(obj) : await browser.storage.sync.clear();
-			await browser.storage.local.set(obj);
+			let working = obj.sync ? browser.storage.sync.set(obj) : browser.storage.sync.clear();
+			working.then(browser.storage.local.set(obj));
 		});
 	});
 });
